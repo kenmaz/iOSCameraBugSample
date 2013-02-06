28447d633e5cc6b9364942f3f400d9b5b9ff31a0
松前 健太郎 <kentaro_matsumae@dwango.co.jp>
2013-02-06 15:03:54 +0900

    uiimagepickerオーバーライド版

--- cameraOrientationSample/ViewController.h
+++ cameraOrientationSample/ViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+- (IBAction)touchButton:(id)sender;
 
 @end
--- cameraOrientationSample/ViewController.m
+++ cameraOrientationSample/ViewController.m
@@ -8,11 +8,65 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface CameraViewController : UIImagePickerController
+@end
+
+@implementation CameraViewController
+
+- (void)viewDidAppear:(BOOL)animated {
+    //[super viewDidAppear:animated]; これがないと画像向きがおかしくなるよ
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.sourceType = UIImagePickerControllerSourceTypeCamera;
+    self.showsCameraControls = NO;
+    self.toolbarHidden = YES;
+    self.navigationBarHidden = YES;
+    
+    CGRect frame = [[UIScreen mainScreen] bounds]; // applicationFrameだと二回目の呼び出しでstatusbar分が入ってしまうので
+    CGFloat toolbarHeight = 54;
+    
+    CGRect toolbarFrame = CGRectMake(0, frame.size.height - toolbarHeight, frame.size.width, toolbarHeight);
+    UIView *toolbar = [[UIView alloc] initWithFrame:toolbarFrame];
+    toolbar.userInteractionEnabled = YES;
+    
+    CGSize shotBtnSize = CGSizeMake(54, 54);
+    UIButton *shotButton = [[UIButton alloc] initWithFrame:CGRectMake(
+                                                                      (toolbarFrame.size.width - shotBtnSize.width) / 2,
+                                                                      0,
+                                                                      shotBtnSize.width,
+                                                                      shotBtnSize.height)];
+    [shotButton setImage:[UIImage imageNamed:@"icon.png"] forState:UIControlStateNormal];
+    CGSize cancelBtnSize = CGSizeMake(44, 32);
+    UIButton *cancelButton = [[UIButton alloc] initWithFrame:CGRectMake(
+                                                                        8,
+                                                                        (toolbarHeight - cancelBtnSize.height) / 2 + 2,
+                                                                        cancelBtnSize.width,
+                                                                        cancelBtnSize.height)];
+    [cancelButton setTitle:@"Cancel" forState:UIControlStateNormal];
+    cancelButton.titleLabel.font = [UIFont systemFontOfSize:12];
+    cancelButton.titleLabel.textColor = [UIColor yellowColor];
+    cancelButton.titleLabel.shadowColor = [UIColor grayColor];
+    
+    [shotButton addTarget:self.delegate action:@selector(takePhoto:) forControlEvents:UIControlEventTouchUpInside];
+    [cancelButton addTarget:self.delegate action:@selector(cancelCamera:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [toolbar addSubview:shotButton];
+    [toolbar addSubview:cancelButton];
+    
+    self.cameraOverlayView = toolbar;
+}
 
 @end
 
-@implementation ViewController
+@interface ViewController ()
+@end
+
+@implementation ViewController {
+    UIImagePickerController* _imagePicker;
+}
 
 - (void)viewDidLoad
 {
@@ -24,6 +78,78 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)touchButton:(id)sender {
+    _imagePicker = [[CameraViewController alloc] init];
+    _imagePicker.delegate = self;
+    
+/*
+    _imagePicker = [[UIImagePickerController alloc] init];
+    _imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    _imagePicker.delegate = self;
+    _imagePicker.showsCameraControls = NO;
+    _imagePicker.toolbarHidden = YES;
+    _imagePicker.navigationBarHidden = YES;
+    
+    CGRect frame = [[UIScreen mainScreen] bounds]; // applicationFrameだと二回目の呼び出しでstatusbar分が入ってしまうので
+    CGFloat toolbarHeight = 54;
+    
+    CGRect toolbarFrame = CGRectMake(0, frame.size.height - toolbarHeight, frame.size.width, toolbarHeight);
+    UIView *toolbar = [[UIView alloc] initWithFrame:toolbarFrame];
+    toolbar.userInteractionEnabled = YES;
+    
+    CGSize shotBtnSize = CGSizeMake(54, 54);
+    UIButton *shotButton = [[UIButton alloc] initWithFrame:CGRectMake(
+                                                                      (toolbarFrame.size.width - shotBtnSize.width) / 2,
+                                                                      0,
+                                                                      shotBtnSize.width,
+                                                                      shotBtnSize.height)];
+    [shotButton setImage:[UIImage imageNamed:@"icon.png"] forState:UIControlStateNormal];
+    CGSize cancelBtnSize = CGSizeMake(44, 32);
+    UIButton *cancelButton = [[UIButton alloc] initWithFrame:CGRectMake(
+                                                                                    8,
+                                                                                    (toolbarHeight - cancelBtnSize.height) / 2 + 2,
+                                                                                    cancelBtnSize.width,
+                                                                                    cancelBtnSize.height)];
+    [cancelButton setTitle:@"Cancel" forState:UIControlStateNormal];
+    cancelButton.titleLabel.font = [UIFont systemFontOfSize:12];
+    cancelButton.titleLabel.textColor = [UIColor yellowColor];
+    cancelButton.titleLabel.shadowColor = [UIColor grayColor];
+    
+    [shotButton addTarget:self action:@selector(takePhoto:) forControlEvents:UIControlEventTouchUpInside];
+    [cancelButton addTarget:self action:@selector(cancelCamera:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [toolbar addSubview:shotButton];
+    [toolbar addSubview:cancelButton];
+    
+    _imagePicker.cameraOverlayView = toolbar;
+*/    
+    [self presentViewController:_imagePicker animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo {
+    NSLog(@"%s", __func__);
+    NSLog(@"image orientation: %d", image.imageOrientation);
+
+    self.imageView.image = image;
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    NSLog(@"%s", __func__);
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void) takePhoto:(id)sender {
+    NSLog(@"%s", __func__);
+    [_imagePicker takePicture];
+}
+
+- (void) cancelCamera:(id)sender {
+    NSLog(@"%s", __func__);
+    [UIApplication sharedApplication].statusBarHidden = NO;
+    [_imagePicker dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
